d2e12eaed851b94037ab361c0765ec12206aea4a
cuixingzheng <cuixingzheng@4502@duia.com>
2020-10-26 11:34:30 +0800

    "alibaba nacos"

---
+++ springcloud2020/springcloudalibaba2020-nacos-config-client/src/main/java/com/springcloudalibaba2020/nacos/config/client/Springcloudalibaba2020NacosConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.springcloudalibaba2020.nacos.config.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springcloudalibaba2020NacosConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springcloudalibaba2020NacosConfigClientApplication.class, args);
+    }
+
+}
--- springcloud2020/springcloudalibaba2020-nacos-order/src/main/java/com/springcloudalibaba2020/nacos/order/springcloudalibaba2020nacosorder/Springcloudalibaba2020NacosOrderApplication.java
+++ springcloud2020/springcloudalibaba2020-nacos-order/src/main/java/com/springcloudalibaba2020/nacos/order/springcloudalibaba2020nacosorder/Springcloudalibaba2020NacosOrderApplication.java
@@ -2,8 +2,10 @@ package com.springcloudalibaba2020.nacos.order.springcloudalibaba2020nacosorder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Springcloudalibaba2020NacosOrderApplication {
 
     public static void main(String[] args) {
